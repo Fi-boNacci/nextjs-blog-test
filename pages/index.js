06e34cddd382340bc6201666e1eb38a6b195e846
Fibonacci <bnachin97@gmail.com>
2021-06-19 17:15:51 +0800

    publisher-iig cdn ees awdag bolgoson

--- pages/index.js
+++ pages/index.js
@@ -12,8 +12,9 @@ import MyNavbar from "components/myNavbar";
 import Intro from "components/intro";
 import ListItem from "components/list-item";
 import GridItem from "components/grid-item";
+import { getAllPosts } from "lib/api";
 
-export default function Home() {
+export default function Home({ posts }) {
   return (
     <Container>
       <MyNavbar />
@@ -23,38 +24,19 @@ export default function Home() {
             <Intro />
           </Col>
         </Row>
-
+        <pre>{JSON.stringify(posts, null, 2)}</pre>
         <hr />
 
         <div className={`page-wrapper`}>
           <Row className="mb-5">
-            <Col md="10">
+            {/* <Col md="10">
               <ListItem />
-            </Col>
-
-            <Col md="4">
-              <GridItem />
-            </Col>
-
-            <Col md="4">
-              <GridItem />
-            </Col>
-
-            <Col md="4">
-              <GridItem />
-            </Col>
-
-            <Col md="4">
-              <GridItem />
-            </Col>
-
-            <Col md="4">
-              <GridItem />
-            </Col>
-
-            <Col md="4">
-              <GridItem />
-            </Col>
+            </Col> */}
+            {posts.map((post) => (
+              <Col md="4">
+                <GridItem post={post} />
+              </Col>
+            ))}
           </Row>
         </div>
       </div>
@@ -70,3 +52,12 @@ export default function Home() {
     </Container>
   );
 }
+
+export const getStaticProps = async () => {
+  const posts = await getAllPosts();
+  return {
+    props: {
+      posts,
+    },
+  };
+};
